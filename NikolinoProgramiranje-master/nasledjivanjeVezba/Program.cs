fcabadae62b63ff954a28abe1aeb3b778940fe1e
Slobodan Mijnaovic <97546009+Sloobex1@users.noreply.github.com>
2022-01-21 10:50:08 +0100

    Add files via upload

---
+++ NikolinoProgramiranje-master/nasledjivanjeVezba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nasledjivanjeVezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ NikolinoProgramiranje-master/nasledjivanjeVezba/Radnik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasledjivanjeVezba
+{
+    class Radnik
+    {
+        public string ime;
+        public string prezime;
+        public string jmbg;
+        public double zarada;
+
+        public Radnik(string ime, string prezime, string jmbg, double zarada)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.jmbg = jmbg;
+            this.zarada = zarada;
+
+        }
+
+        public double godisnjaZarada()
+        {
+            return 12 * zarada;
+        }
+
+    }
+    
+}
